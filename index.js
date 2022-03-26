54e443acf3d89564c6468d915c5202d8226595ef
morehwachege <antony123muriithi@gmail.com>
2022-03-26 22:52:31 +0300

    remove node modules from git, add response todo.ejs

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-const expess = require("express");
+const express = require("express");
 const app = express();
+const port = 3000;
+const host = `https://localhost:${port}`
+
+app.use('/static', express.static("public"))
+app.use(express.urlencoded({extended: true }));
+
+app.set("view engine", "ejs");
+
+app.get('/', (req,res) => {
+    res.render('todo.ejs');
+})
+app.post('/', (req, res) => {
+    console.log(req.body);
+})
+
+app.listen(port, console.log(`Server listening at ${host}`))
